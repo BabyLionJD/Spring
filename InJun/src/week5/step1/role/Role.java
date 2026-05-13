b9c96ede3b2e369942f9ed5318824905250a35b7
인준 <cij041109@gmail.com>
2026-05-13 13:39:49 +0900

    feat: 5주차 Step1 역할과 정책 구조 구현

---
+++ InJun/src/week5/step1/role/Role.java
@@ -0,0 +1,42 @@
+package week5.step1.role;
+
+import week5.step1.policy.Policy;
+
+public abstract class Role {
+
+    private String name;
+    private String major;
+    private int cd;
+    private String part;
+
+    public Role(String name, String major, int cd, String part) {
+        this.name = name;
+        this.major = major;
+        this.cd = cd;
+        this.part = part;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public int getCd() {
+        return cd;
+    }
+
+    public String getPart() {
+        return part;
+    }
+
+    public abstract Policy getPolicy();
+
+    public abstract String getDetailInfo();
+
+    public boolean submit() {
+        return getPolicy().checkSub();
+    }
+}
