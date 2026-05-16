9cb71ffa390e28d1a4a6da47e00ef38cf06de1c6
parkc31 <pc0301pc@gmail.com>
2026-05-16 21:40:52 +0900

    Week7:REST API 설계(CRUD)

---
+++ Chan/src/main/java/com/BabyLion/Spring/dto/StaffResponse.java
@@ -0,0 +1,48 @@
+package com.BabyLion.Spring.dto;
+
+import com.BabyLion.Spring.domain.role.Staff;
+
+public class StaffResponse {
+    private String name;
+    private String major;
+    private int generation;
+    private String part;
+    private String roleName;
+    private String position;
+
+    public static StaffResponse from(Staff staff) {
+        StaffResponse dto = new StaffResponse();
+        dto.name = staff.getName();
+        dto.major = staff.getMajor();
+        dto.generation = staff.getGeneration();
+        dto.part = staff.getPart();
+        dto.roleName = staff.roleName();
+        dto.position = staff.getPosition();
+        return dto;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public int getGeneration() {
+        return generation;
+    }
+
+    public String getPart() {
+        return part;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
