21b258e0366b7a67cd0c4373c55ffe4341ed683b
인준 <cij041109@gmail.com>
2026-07-25 11:32:45 +0900

    fix: AssignmentController RequestParam import 추가

--- InJun/pbl/src/main/java/com/likelion/pbl/week11/assignment/controller/AssignmentController.java
+++ InJun/pbl/src/main/java/com/likelion/pbl/week11/assignment/controller/AssignmentController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class AssignmentController {
